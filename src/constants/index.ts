02db75d153a3dc14a727ffbca679d4e3131cc374
fuelsgate <fuelsgate.fuelsgate@gmail.com>
2024-11-26 10:16:09 +0100

    reorganized constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,6 +4,6 @@ export const TOKEN_SECRET = process.env.JWT_SECRET;
 export const EMAIL_HOST = process.env.EMAIL_HOST;
 export const EMAIL_USERNAME = process.env.EMAIL_USERNAME;
 export const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD;
+export const PORT = process.env.PORT;
 export const EMAIL_PORT = parseInt(process.env.EMAIL_PORT);
 export const FRONTEND_URL = process.env.FRONTEND_URL;
-export const PORT = process.env.PORT;
